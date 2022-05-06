1b0aff3fd9e24290d3b1b6c2908b28de5de2845e
Cyprien Siaud <74303569+CSIAUD@users.noreply.github.com>
2022-05-06 15:31:22 +0200

    Update myWifi.h
    Ajout connexion en wifiMulti

--- myWifi.h
+++ myWifi.h
@@ -4,6 +4,7 @@
  * @brief Configuration de l'access point
  * Uilisation de la biliothèque <WiFi.h>
  * Uilisation de la biliothèque <WiFiMulti.h>
+ * 
  * Fonction de connexion à un wifi
  * Fonction de sauvegarde d'un wifi
  * 
@@ -12,20 +13,46 @@
 #include <WiFi.h>
 #include <WiFiMulti.h>
 
-char ap_ssid[32]; // SSID du AP pour la config
-char ap_pwd[64]; // MDP du AP pour la config
+WiFiMulti wifiMulti; 
 
+/**
+ * @brief Tente la connexion en WifiMulti.
+ * Get l'objet wifis dans le fichier de SPIFFS
+ * Si au moins 1 wifis de sauvegardé tentative de connexion
+ */
+void multiConnect(){
+    JsonObject wifis = spiffsGet("wifis");
+    if(wifis.size() > 0){
+        for(JsonVariant wifi : wifis){
+            wifiMulti.add(
+                    wifi.key().c-str(), // Le SSID
+                    wifi.value().as<const char*>() // Le MDP
+                );
+        }
+
+        if(wifiMulti.run() == WL_CONNECTED) { // Connexion
+            MYDEBUG_PRINTLN("");
+            MYDEBUG_PRINTLN("WiFi connecté");
+            MYDEBUG_PRINTLN("@IP : ");
+            MYDEBUG_PRINTLN(WiFi.localIP());
+        }else{
+            MYDEBUG_PRINTLN("Aucun réseau connu à proximité");
+        }
+    }
+}
+
+/**
+ * @brief Démarre l'access point à partir du fichier SPIFFS
+ * 
+ * 
+ */
 void setupWifi(){
     JsonObject obj = spiffsGet("config");
-    // String ssid = obj["ssid"];
-    // ssid.toCharArray(ap_ssid,sizeof(ssid)+1);
-    // String pwd = obj["pwd"];
-    // pwd.toCharArray(ap_pwd,sizeof(pwd)+1);
-
     WiFi.mode(WIFI_AP_STA);
-    // WiFi.softAP(ap_ssid, ap_pwd);
     WiFi.softAP(obj["ssid"], obj["pwd"]);
 
     MYDEBUG_PRINT("@IP de la carte : ");
     MYDEBUG_PRINTLN(WiFi.softAPIP());
+
+    multiConnect();
 }
